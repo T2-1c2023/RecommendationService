6c3a13316264071b904ccde1cf36cbb4d73f0dd7
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2023-06-04 17:07:51 -0300

    Base de datos en Mongo Atlas

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/T2-1c2023/RecommendationService/app/controller"
@@ -13,9 +14,14 @@ import (
 	"github.com/T2-1c2023/RecommendationService/app/services"
 	config "github.com/T2-1c2023/RecommendationService/config"
 	_ "github.com/T2-1c2023/RecommendationService/docs"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	client, err := config.SetUpDB()
 	if err != nil {
 		panic(err)
